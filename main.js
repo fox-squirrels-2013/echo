273e3d0bc74a71df03ad699b148d3d4beb31783c
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-12-12 16:21:46 -0800

    code from handlebars lecture

--- main.js
+++ main.js
@@ -1,15 +1,56 @@
-var MyController = {
-  echoParams: {
-    url: '/echo',
-    type: 'get',
-    data: 'p1=123&p2[a]=456&p2[b]=789' // don't forget to explain this
+var Templates = {
+  _cachedTemplates: {},
+  render: function(name, data, callback) {
+    var source = this._cachedTemplates[name]
+    if (source) {
+      console.log("template is cached, using cached version")
+      this._processTemplate(source, data, callback)
+    } else {
+      console.log("template is NOT cached, getting it from server")
+      var self = this
+      $.get("/templates/" + name + ".hbs")
+      .done(function(source) {
+        console.log(this) // this === the jQuery AJAX object
+        console.log(self) // self === Templates 
+        self._cachedTemplates[name] = source
+        self._processTemplate(source, data, callback)
+      })
+    }
   },
-  echoDone: function(serverData) {
-    console.log("server data", serverData)
+  _processTemplate: function(source, data, callback) {
+    var template = Handlebars.compile(source)
+    callback(template(data))
   }
 }
 
-$(function() {
-  $.ajax(MyController.echoParams).done(MyController.echoDone)
-})
+function f() {
+  console.log(this)
+  function g() {
+    console.log(this)
+    function h() {
+      console.log(this)
+    }
+    h()
+  }
+  g()
+}
 
+
+function replaceContainer(renderedTemplate) {
+  $('.container').html(renderedTemplate)
+}
+
+$(function() {
+
+  f()
+
+  $.get("/data", function(data) {
+    Templates.render("homepage", data, replaceContainer)
+    setTimeout(function() {
+      Templates.render("homepage", data, replaceContainer)
+      Templates.render("homepage", data, replaceContainer)
+      Templates.render("homepage", data, replaceContainer)      
+    }, 2000)
+  })
+  
+})
--- server.js
+++ server.js
@@ -6,8 +6,22 @@ var express = require('express'),
 var port = process.env.PORT || 8080
 console.log("Listening on port", port)
 server.listen(port)
-app.get('/echo', echo)
 app.use(express.static(__dirname))
+app.get('/echo', echo)
+app.get('/data', returnData)
+
+
+function returnData(req, res) {
+  var data = {
+    name: "Jeffrey",
+    age: 40,
+    isAnnoying: true
+  }
+  var body = JSON.stringify(data);
+  res.setHeader('Content-Type', 'application/json')
+  res.setHeader('Content-Length', body.length)
+  res.end(body)
+}
 
 function echo(req, res) {
   var body = JSON.stringify(req.query);
