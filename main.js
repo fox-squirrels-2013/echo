dcc93624eeb7f7d75c7a3fe5624f8f32ad8b4dd0
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-12-17 13:27:46 -0800

    Basic framework in place: - on page load, the list of users will be displayed - clicking on a user will load that user and display an edit form

--- main.js
+++ main.js
@@ -1,15 +1,48 @@
-var MyController = {
-  echoParams: {
-    url: '/echo',
-    type: 'get',
-    data: 'p1=123&p2[a]=456&p2[b]=789' // don't forget to explain this
+var UsersController = {
+  init: function() {
+    $('.container').on('click', '.user-list a', this.getAndRenderUserForm.bind(this))
+    // TODO: bind additional event(s) to handle partial updates
+    this.getAndRenderUsers()
   },
-  echoDone: function(serverData) {
-    console.log("server data", serverData)
+
+  getAndRenderUsers: function() {
+    $.get('/users', this.renderUserLinks.bind(this))
+  },
+
+  renderUserLinks: function(users) {
+    for (var i in users) {
+      this.renderUserLink(users[i])
+    }
+  },
+
+  renderUserLink: function(user) {
+    var $userLink = $($('#user-link').html())
+    $userLink.find('a').attr('href', '/users/' + user.id).data('id', user.id).html(user.name)
+    $('.user-list').append($userLink)
+  },
+
+  getAndRenderUserForm: function(e) {
+    e.preventDefault()
+    var $userLink = $(e.target)
+    $.get($userLink.attr('href'), this.renderUserForm.bind(this))
+  },
+
+  renderUserForm: function(user) {
+    var $userForm = $($('#user-form').html())
+    // we'll use PATCH when we submit via AJAX, but browser needs POST
+    $userForm.attr('action', '/users/' + user.id).attr('method', 'post')
+    $userForm.find('input[name=name]').val(user.name)
+    $userForm.find('input[name=age]').val(parseInt(user.age))
+    $userForm.find('input[name=sex][value='+user.sex+']').attr('checked', true)
+    $('.edit-area').html('').append($userForm)
+  },
+
+  patchUser: function(e) {
+    // TODO: implement the code to update the user on the backend
   }
 }
 
 $(function() {
-  $.ajax(MyController.echoParams).done(MyController.echoDone)
+  UsersController.init()
 })
 
--- server.js
+++ server.js
@@ -6,13 +6,59 @@ var express = require('express'),
 var port = process.env.PORT || 8080
 console.log("Listening on port", port)
 server.listen(port)
-app.get('/echo', echo)
 app.use(express.static(__dirname))
+app.use(express.bodyParser())
+app.get('/echo', echo)
+app.get('/users', getUsers)
+app.get('/users/:id', getUser)
+app.patch('/users/:id', updateUser)
 
 function echo(req, res) {
   var body = JSON.stringify(req.query);
   res.setHeader('Content-Type', 'application/json')
-  // res.setHeader('Content-Type', 'text/plain')
-  res.setHeader('Content-Length', body.length)
   res.end(body)
+}
+
+
+// Fake user data and simplistic "GET" and "PATCH handlers"
+var Users = {
+  1: { id: 1, name: 'Anita Neujobb', sex: 'F', age: '26' },
+  2: { id: 2, name: 'Ivanna Lernkoding', sex: 'F', age: '32' },
+  3: { id: 3, name: 'Ben Hackinalot', sex: 'M', age: '29' }
+}
+
+function getUsers(req, res) {
+  res.setHeader('Content-Type', 'application/json')
+  var body = JSON.stringify(Users)
+  res.status(200).end(body)
+}
+
+function getUser(req, res) {
+  res.setHeader('Content-Type', 'application/json')
+  var userId = req.params['id']
+  if (userId > Users.length-1) {
+    userNotFound(req, res, userId)
+  } else {
+    var body = JSON.stringify(Users[userId])
+    res.status(200).end(body)
+  }
+}
+
+function updateUser(req, res) {
+  res.setHeader('Content-Type', 'application/json')
+  var userId = req.params['id']
+  if (userId > Users.length-1) {
+    userNotFound(req, res, userId)
+  } else {
+    for (var k in req.body) {
+      Users[userId][k] = req.body[k]
+    }
+    var body = JSON.stringify(Users[userId])
+    res.status(200).end(body)
+  }
+}
+
+function userNotFound(req, res, userId) {
+  var body = JSON.stringify({ message: "No user with id " + userId + " exists." })
+  res.status(404).end(body)
 }
