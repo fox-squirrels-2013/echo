484d145fc0a84c17dcd1b8d8b7f66b90ed368f96
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-12-18 11:38:35 -0800

    Simple server that uses: - World Weather Online API (http://developer.worldweatheronline.com/) - Socket.io

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 var express = require('express'),
+    sio     = require('socket.io'),
     app     = express(),
-    server  = require('http').createServer(app)
+    server  = require('http').createServer(app),
+    request = require('request'),
+    io      = sio.listen(server)
 
 
 var port = process.env.PORT || 8080
 console.log("Listening on port", port)
 server.listen(port)
-app.get('/echo', echo)
+
+// express routes
 app.use(express.static(__dirname))
+app.get('/echo', echo)
 
 function echo(req, res) {
   var body = JSON.stringify(req.query);
@@ -15,4 +20,49 @@ function echo(req, res) {
   // res.setHeader('Content-Type', 'text/plain')
   res.setHeader('Content-Length', body.length)
   res.end(body)
+}
+
+
+// socket.io handlers
+var numClients = 0
+io.sockets.on('connection', clientConnect)
+function clientConnect(socket) {
+  numClients++
+  checkSFWeatherAndUpdateClients()
+}
+
+
+// keep calling the weather API to update the current temperature
+if (!process.env.WWO_API_KEY) {
+  console.log('ERROR: WWO_API_KEY must be set in your environment.')
+  process.exit()
+}
+
+var lastTemperature, lastNumClients
+checkSFWeatherAndUpdateClients()
+
+function checkSFWeatherAndUpdateClients() {
+  var url = 'http://api.worldweatheronline.com/free/v1/weather.ashx?q=San%20Francisco%2C%20CA&format=JSON&extra=&num_of_days=&date=&fx=&cc=&includelocation=&show_comments=&key=' + process.env.WWO_API_KEY
+
+  request(url, function(error, response, body) {
+    updateClientsAndScheduleNextCheck(JSON.parse(body))
+  })
+}
+
+function updateClientsAndScheduleNextCheck(weatherData) {
+  console.log(weatherData.data)
+  var currentTemperature = parseInt(weatherData.data.current_condition[0].temp_F)
+  // if the temperature has changed or new clients have connected, notify them
+  if ((currentTemperature != lastTemperature) || (lastNumClients < numClients)) {
+    lastTemperature = currentTemperature
+    lastNumClients = numClients
+    io.sockets.emit('temperature', {
+      location: weatherData.data.request[0].query,
+      temp_F: weatherData.data.current_condition[0].temp_F,
+      temp_C: weatherData.data.current_condition[0].temp_C,
+      when: (new Date())
+    })
+  }
+
+  setTimeout(checkSFWeatherAndUpdateClients, 60000)
 }
